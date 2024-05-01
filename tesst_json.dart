71b1999bea95cec41c1035c6e2a9b258805f8614
Maher Douba <maherdouba@ariya-tech.net>
2024-05-01 14:21:17 +0430

    added exception handling

--- tesst_json.dart
+++ tesst_json.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:io';
 import 'package:http/http.dart' as http;
 
 class User {
@@ -31,19 +32,35 @@ class User {
 void main() async {
   final url = 'webcode.me';
   final path = '/users.json';
-  try {
-    final resp = await http.get(Uri.http(url, path));
 
-    if (resp.statusCode == 200) {
-      List<dynamic> data = json.decode(resp.body)['users'];
-      List<User> users =
-          List<User>.from(data.map<User>((dynamic e) => User.fromJson(e)));
-      print(users[0]);
-      print('encode: ${json.encode(users[0])}');
-    } else {
-      print('Failed to load data: ${resp.statusCode}');
+ exception exc = exception();
+ exc.fetch_data(url,path);
+  
+}
+
+class exception {
+Future<void> fetch_data(String url,String path) async{
+    try {
+      final resp = await http.get(Uri.http(url, path));
+    
+      if (resp.statusCode == 200) {
+        List<dynamic> data = json.decode(resp.body)['users'];
+        List<User> users =
+            List<User>.from(data.map<User>((dynamic e) => User.fromJson(e)));
+        print(users[0]);
+        print('encode: ${json.encode(users[0])}');
+      } else {
+        print('Failed to load data: ${resp.statusCode}');
+      }
+    }on SocketException catch  (e) {
+      print('an internet error occurred: $e');
+    }on HttpException catch(e){
+      print('an error occurred in the url address: $e');
+    }on FormatException catch(e){
+      print('a data format error occurred: $e');
+    }catch(e){
+      print('something went wrong : $e');
     }
-  } catch (e) {
-    print('An error occurred: $e');
   }
+  
 }
